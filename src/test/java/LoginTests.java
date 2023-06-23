f23223aed50839e5d144895756b76d900528ec20
camoraga10 <carlos.moraga@testpro.io>
2023-06-23 14:28:20 -0500

    Registration test
    Code for registration test

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -12,7 +12,7 @@ public class LoginTests extends BaseTest {
 
 
 
-        
+
 //      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
