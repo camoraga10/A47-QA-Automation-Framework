ea472459e2455351e3b5ead1d6ca7ea52ac13d2c
camoraga10 <carlos.moraga@testpro.io>
2023-06-23 20:26:05 -0500

    registration test

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -23,6 +25,31 @@ public class LoginTests extends BaseTest {
         String url = "https://qa.koel.app/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
+        driver.quit();
+    }
+}
+public class Homework16 {
+
+    @Test
+    public void registrationNavigation() {
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+
+        // Create a new instance of the ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate to "https://qa.koel.app/"
+        driver.get("https://qa.koel.app/");
+
+        // Click the Registration link
+        WebElement registrationLink = driver.findElement(By.linkText("Registration"));
+        registrationLink.click();
+
+        // Verify that you are redirected to the Registration page using Assert
+        String expectedTitle = "Koel - Registration";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(expectedTitle, actualTitle);
+        // Close the browser
         driver.quit();
     }
 }
