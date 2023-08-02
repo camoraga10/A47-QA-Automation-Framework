cec05622c2588184c600df3e9bc96d465b46e4f2
camoraga10 <carlos.moraga@testpro.io>
2023-08-02 08:53:40 -0500

    login
    graddle

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,13 +1,28 @@
+import com.beust.jcommander.Parameters;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
-public class BaseTest {
+public static String url = "https://qa.koel.app/";
 
     @BeforeSuite
-    static void setupClass() {
+static void setupClas(){
         WebDriverManager.chromedriver().setup();
     }
+    @BeforeMethod
+@Parameters({"BaseURL"})
+public void launchBrowser(String BaseURL){
+        ChromeOptions options=new ChromeOptions();
+        opetions.addArguments("--remote-allow-origins=*");
+
+        driver=new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofseconds(10));
+
+        url=BaseURL;
+        driver.get(url);
+        }
 
 
-}
---
+++ src/test/java/homework18.java
@@ -0,0 +1,7 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+public class homework18 {
+    @BeforeMethod
+    @Parameters()
+}
